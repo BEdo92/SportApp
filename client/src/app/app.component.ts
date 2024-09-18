8c8bfd4bf3997f9f462d8d96b88515ae8467c128
edinabonifert <edinabonifert@gmail.com>
2024-09-18 17:56:02 +0200

    Register form created.

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,26 +1,23 @@
 import { CommonModule, NgFor } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
-import { createInjectableType } from '@angular/compiler';
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from "./nav/nav.component";
 import { AccountService } from './_services/account.service';
+import { HomeComponent } from "./home/home.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, CommonModule, NavComponent]
+    imports: [RouterOutlet, CommonModule, NavComponent, HomeComponent]
 })
 export class AppComponent implements OnInit {
-  http = inject(HttpClient);
   private accountService = inject(AccountService);
   title = 'Sports meter';
-  users: any;
+
 
   ngOnInit(): void {
-    this.getUsers();
     this.setCurrentUser();
   }
 
@@ -33,12 +30,6 @@ export class AppComponent implements OnInit {
     this.accountService.currentUser.set(user);
   }
 
-  getUsers() {
-    this.http.get('https://localhost:5001/api/users').subscribe({
-      next: response => this.users = response,
-      error: error => console.log(error),
-      complete: () => console.log('Request has completed')
-    })
-  }
+ 
 
 }
